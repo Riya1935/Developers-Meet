75514b322559cae848f4493dbb7b24e5d762f30c
Riya Kumari <riyathakur1935@gmail.com>
2020-12-20 13:08:30 +0530

    Added code to connect our application with mongodb database using mongoose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express=require('express');
-
+const connectDB = require('./config/db.js');
 
 
 const app=express();
+
+connectDB();
+
 const PORT=process.env.PORT || 5000;
 
 app.get('/', (req,res)=> res.send('Server running!'));
